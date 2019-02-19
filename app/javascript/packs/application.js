3935fbf61dfbcfffef4f292accfd8bc618e6e9cb
金正朋也 <mhtnim1023@gmail.com>
2019-02-19 14:50:37 +0900

    vue-routerのinstall

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from '../../javascript/app.vue'
 import Vuetify from 'vuetify'
+import VueRouter from 'vue-router'
 import store from '../packs/store.js'
 import 'vuetify/dist/vuetify.min.css'
 import '@fortawesome/fontawesome-free/css/all.css'
@@ -9,6 +10,8 @@ import '@fortawesome/fontawesome-free/css/all.css'
 Vue.use(Vuetify, {
   iconfont: 'fa'  // fontawesomeを使うために指定
 })
+
+Vue.use(VueRouter)
 
 // loadされると、以下のjsが発火する
 document.addEventListener('DOMContentLoaded', () => {
